b1654fb1ae197ebed55c4dab89148b6ad33ffb49
Sgoddev <sgod.dev009@gmail.com>
2024-05-31 12:09:36 +0700

    feat(admin): CURD Roles

--- client/src/pages/apps/permissions/index.tsx
+++ client/src/pages/apps/permissions/index.tsx
@@ -1,5 +1,4 @@
-// ** React Imports
-import { useState, useEffect, useCallback, FormEvent } from 'react'
+import { useState, useCallback, FormEvent, ReactElement, Ref, forwardRef, Fragment } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
@@ -21,9 +20,6 @@ import FormControlLabel from '@mui/material/FormControlLabel'
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
 
-// ** Store Imports
-import { useDispatch } from 'react-redux'
-
 // ** Custom Components Imports
 import CustomChip from 'src/@core/components/mui/chip'
 import PageHeader from 'src/@core/components/page-header'
@@ -31,44 +27,51 @@ import TableHeader from 'src/views/apps/permissions/TableHeader'
 import CustomTextField from 'src/@core/components/mui/text-field'
 
 // ** Types Imports
-import { AppDispatch } from 'src/store'
 import { RolesRowType } from 'src/types/apps/roleTypes'
-
-// ** Actions Imports
-import { fetchData } from 'src/store/apps/permissions'
 
 // ** Types
 import { ThemeColor } from 'src/@core/layouts/types'
 
 // ** Import third party
 import { useTranslation } from 'react-i18next'
-import useSWR from 'swr'
+import useSWR, { mutate } from 'swr'
 
 // ** Service
 import masterAdminService from 'src/service/masterAdmin.service'
 
 // ** Permission
 import permissions from '../../../configs/permissions.json'
+import toast from 'react-hot-toast'
+import {
+  SlideProps,
+  Slide,
+  DialogContentText,
+  DialogActions,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from '@mui/material'
 
 interface Colors {
   [key: string]: ThemeColor
 }
-
 interface CellType {
   row: RolesRowType
 }
 
 const colors: Colors = {
-  support: 'info',
-  users: 'success',
-  manager: 'warning',
-  administrator: 'primary',
-  'restricted-user': 'error'
+  CREATE: 'success',
+  UPDATE: 'warning',
+  READ: 'primary',
+  DELETE: 'error'
 }
 
 const defaultColumns: GridColDef[] = [
   {
-    flex: 0.25,
+    flex: 0,
     field: 'name',
     minWidth: 240,
     headerName: 'Roles Name',
@@ -82,7 +85,8 @@ const defaultColumns: GridColDef[] = [
     renderCell: ({ row }: CellType) => {
       return row.permissionID.map((permissionID: any, index: number) => {
         const permission = permissions.find((p: { id: number }) => p.id === permissionID)
-        const permissionName = permission ? permission.namePermission : 'Unknown'
+        const permissionAction = permission ? permission.action.toUpperCase() : 'UNKNOWN'
+        const permissionName = permission ? permission.namePermission.toUpperCase() : 'UNKNOWN'
 
         return (
           <CustomChip
@@ -90,75 +94,151 @@ const defaultColumns: GridColDef[] = [
             size='small'
             key={index}
             skin='light'
-            color={colors[permissionName]}
-            label={permissionName.replace('-', ' ')}
-            sx={{ '& .MuiChip-label': { textTransform: 'capitalize' }, '&:not(:last-of-type)': { mr: 3 } }}
+            color={colors[permissionAction]}
+            label={permissionName}
+            sx={{
+              '& .MuiChip-label': { textTransform: 'capitalize' },
+              '&:not(:last-of-type)': { mr: 3 }
+            }}
           />
         )
       })
     }
   }
-
-  // {
-  //   flex: 0.25,
-  //   minWidth: 210,
-  //   field: 'createdDate',
-  //   headerName: 'Created Date',
-  //   renderCell: ({ row }: CellType) => <Typography sx={{ color: 'text.secondary' }}>{row.createdDate}</Typography>
-  // }
 ]
 
+const Transition = forwardRef(function Transition(
+  props: SlideProps & { children?: ReactElement<any, any> },
+  ref: Ref<unknown>
+) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+const DeleteConfirmationDialog = ({
+  open,
+  onClose,
+  onConfirm,
+  t
+}: {
+  open: boolean
+  onClose: () => void
+  onConfirm: () => void
+  t: (key: string) => string
+}) => {
+  return (
+    <Dialog open={open} onClose={onClose} TransitionComponent={Transition}>
+      <DialogTitle>{t('Xác nhận xóa')}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>{t('Bạn có chắc chắn muốn xóa vai trò này không?')}</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>{t('Hủy')}</Button>
+        <Button onClick={onConfirm} color='error' autoFocus>
+          {t('Xóa')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
 const PermissionsTable = () => {
-  // ** State
   const [value, setValue] = useState<string>('')
   const [editValue, setEditValue] = useState<string>('')
   const [editDialogOpen, setEditDialogOpen] = useState<boolean>(false)
   const [paginationModel, setPaginationModel] = useState({ page: 0, pageSize: 10 })
   const { t } = useTranslation()
   const { data: roles } = useSWR('GET_ALL_ROLES', masterAdminService.getAllRole)
-  console.log(roles?.data)
-
-  // ** Hooks
-  const dispatch = useDispatch<AppDispatch>()
-
-  useEffect(() => {
-    dispatch(
-      fetchData({
-        q: value
-      })
-    )
-  }, [dispatch, value])
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false)
+  const [roleId, setRoleId] = useState<string | null>(null)
+  const [rolePermissions, setRolePermissions] = useState<number[]>([])
 
   const handleFilter = useCallback((val: string) => {
     setValue(val)
   }, [])
 
-  const handleEditPermission = (name: string) => {
-    setEditValue(name)
+  const permissionsBySubject = permissions.reduce((acc: any, permission: any) => {
+    const { subject } = permission
+    if (!acc[subject]) {
+      acc[subject] = []
+    }
+    acc[subject].push(permission)
+
+    return acc
+  }, {})
+
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const permissionId = Number(event.target.name)
+    setRolePermissions(prevState =>
+      prevState.includes(permissionId) ? prevState.filter(id => id !== permissionId) : [...prevState, permissionId]
+    )
+  }
+
+  const handleEditPermission = async (role: RolesRowType) => {
+    setRoleId(role._id)
+    setEditValue(role.name)
+    setRolePermissions(role.permissionID.map(Number))
     setEditDialogOpen(true)
+  }
+
+  const handleDeleteRole = (id: string) => {
+    setRoleId(id)
+    setOpenDeleteDialog(true)
+  }
+
+  const handleConfirmDelete = async () => {
+    if (roleId) {
+      try {
+        await masterAdminService.deleteRole({ id: roleId })
+        toast.success(t('Vai trò đã được xóa'))
+        mutate('GET_ALL_ROLES')
+      } catch (error: any) {
+        toast.error(t('Có lỗi xảy ra'))
+      }
+    }
+    setOpenDeleteDialog(false)
+    setRoleId(null)
+  }
+
+  const handleConfirmUpdate = async () => {
+    try {
+      await masterAdminService.updateRole({ _id: roleId, name: editValue, permissionID: rolePermissions })
+      toast.success(t('Vai trò đã được cập nhật'))
+      mutate('GET_ALL_ROLES')
+    } catch (error: any) {
+      toast.error(t('Có lỗi xảy ra'))
+    }
+    setEditDialogOpen(false)
+  }
+
+  const handleCancelDelete = () => {
+    setOpenDeleteDialog(false)
+    setRoleId(null)
   }
 
   const handleDialogToggle = () => setEditDialogOpen(!editDialogOpen)
 
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    setEditDialogOpen(false)
     e.preventDefault()
+    handleConfirmUpdate()
   }
+
+  const filteredRoles =
+    roles?.data.filter((role: RolesRowType) => role.name.toLowerCase().includes(value.toLowerCase())) ?? []
 
   const columns: GridColDef[] = [
     ...defaultColumns,
     {
-      flex: 0.15,
+      flex: 0,
       minWidth: 120,
       sortable: false,
       field: 'actions',
       headerName: 'Actions',
       renderCell: ({ row }: CellType) => (
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <IconButton onClick={() => handleEditPermission(row.name)}>
+          <IconButton onClick={() => handleEditPermission(row)}>
             <Icon icon='tabler:edit' />
           </IconButton>
-          <IconButton>
+          <IconButton onClick={() => handleDeleteRole(row._id)}>
             <Icon icon='tabler:trash' />
           </IconButton>
         </Box>
@@ -173,17 +253,17 @@ const PermissionsTable = () => {
           <PageHeader
             title={
               <Typography variant='h4' sx={{ mb: 6 }}>
-                {t('Danh sách quyền')}
+                {t('Danh sách vai trò')}
               </Typography>
             }
           />
         </Grid>
         <Grid item xs={12}>
           <Card>
-            <TableHeader value={value} handleFilter={handleFilter} />
+            <TableHeader value={value} handleFilter={handleFilter} mutateData={() => mutate('GET_ALL_ROLES')} />
             <DataGrid
               autoHeight
-              rows={roles?.data.map((item: { _id: any }) => ({ ...item, id: item._id })) ?? []}
+              rows={filteredRoles.map((item: { _id: any }) => ({ ...item, id: item._id }))}
               columns={columns}
               disableRowSelectionOnClick
               pageSizeOptions={[10, 25, 50]}
@@ -193,7 +273,13 @@ const PermissionsTable = () => {
           </Card>
         </Grid>
       </Grid>
-      <Dialog maxWidth='sm' fullWidth onClose={handleDialogToggle} open={editDialogOpen}>
+      <Dialog
+        maxWidth='lg'
+        fullWidth
+        onClose={handleDialogToggle}
+        open={editDialogOpen}
+        TransitionComponent={Transition}
+      >
         <DialogTitle
           sx={{
             textAlign: 'center',
@@ -201,10 +287,9 @@ const PermissionsTable = () => {
             pt: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
           }}
         >
-          <Typography variant='h5' component='span' sx={{ mb: 2 }}>
-            Edit Permission
+          <Typography variant='h2' component='span' sx={{ mb: 2 }}>
+            {t('Cập nhật vai trò')}
           </Typography>
-          <Typography variant='body2'>Edit permission as per your requirements.</Typography>
         </DialogTitle>
         <DialogContent
           sx={{
@@ -212,30 +297,68 @@ const PermissionsTable = () => {
             pb: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
           }}
         >
-          <Alert severity='warning' sx={{ maxWidth: '500px' }}>
-            <AlertTitle>Warning!</AlertTitle>
-            By editing the permission name, you might break the system permissions functionality. Please ensure you're
-            absolutely certain before proceeding.
+          <Alert severity='warning' sx={{ maxWidth: '1200px' }}>
+            <AlertTitle>{t('Cảnh báo!')}</AlertTitle>
+            {t(
+              'Bằng cách chỉnh sửa vai trò, bạn có thể phá vỡ chức năng cấp quyền của hệ thống. Hãy đảm bảo bạn hoàn toàn chắc chắn trước khi tiếp tục.'
+            )}
           </Alert>
           <Box component='form' sx={{ mt: 8 }} onSubmit={onSubmit}>
             <FormGroup sx={{ mb: 2, alignItems: 'center', flexDirection: 'row', flexWrap: ['wrap', 'nowrap'] }}>
               <CustomTextField
                 fullWidth
                 value={editValue}
-                label='Permission Name'
-                sx={{ mr: [0, 4], mb: [3, 0] }}
-                placeholder='Enter Permission Name'
+                label={t('Tên vai trò')}
+                sx={{ mr: [0, 5], mb: [3, 0] }}
+                placeholder={t('Nhập tên vai trò') || ''}
                 onChange={e => setEditValue(e.target.value)}
               />
-
-              <Button type='submit' variant='contained' sx={{ mt: 4 }}>
-                Update
+              <Button fullWidth type='submit' variant='contained' sx={{ mt: 4 }}>
+                {t('Cập nhật')}
               </Button>
             </FormGroup>
-            <FormControlLabel control={<Checkbox />} label='Set as core permission' />
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Permission</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {Object.entries(permissionsBySubject).map(([subject, permissions]) => (
+                    <TableRow key={subject}>
+                      <TableCell>{subject.toUpperCase()}</TableCell>
+                      {permissions.map((permission: any) => (
+                        <TableCell key={permission.id}>
+                          <FormGroup row>
+                            <FormControlLabel
+                              control={
+                                <Checkbox
+                                  name={permission.id.toString()}
+                                  checked={rolePermissions.includes(permission.id)}
+                                  onChange={handleCheckboxChange}
+                                />
+                              }
+                              label={permission.namePermission}
+                            />
+                          </FormGroup>
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
           </Box>
         </DialogContent>
       </Dialog>
+      <DeleteConfirmationDialog
+        open={openDeleteDialog}
+        onClose={handleCancelDelete}
+        onConfirm={handleConfirmDelete}
+        t={t}
+      />
     </>
   )
 }
